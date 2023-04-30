e342d8da4c82bdc5cd6221b2697c8dcbf0af3c73
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-04-30 04:41:57 -0300

    refactor: added Bookish header to web site

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Typography } from '@mui/material';
 function App() {
   return (
     <div>
-      <Typography variant='h2' component='h2' data-test='heading' >
-        Hello world
-      </Typography>
+      <Typography variant='h2' component='h2' data-test='heading' >Bookish</Typography>
     </div>
   );
 }
