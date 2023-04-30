9af9e74e450a39a2674665995729051bae3de238
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-04-30 04:55:06 -0300

    feat: book list test passing with minimal implemented

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { Typography } from '@mui/material';
 function App() {
   return (
     <div>
-      <Typography variant='h2' component='h2' data-test='heading' >Bookish</Typography>
+      <Typography variant='h2' component='h2' data-test='heading' >
+        Bookish
+      </Typography>;
+      <div data-test='book-list'>
+        <div className="book-item"></div>
+        <div className="book-item"></div>
+      </div>
     </div>
   );
 }
