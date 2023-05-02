4acf71c26d9af695f7b53f8857d35cd211389c7e
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-05-02 12:03:43 -0300

    feat: improved

--- cypress/e2e/ui/bookish.cy.js
+++ cypress/e2e/ui/bookish.cy.js
@@ -6,11 +6,11 @@ describe('Bookish application', function () {
     it('Shows a book list', () => {
         cy.visit('http://localhost:3000/');
         cy.get('div[data-test="book-list"]').should('exist');
-        cy.get('div.book-item').should('have.length', 2);
+        cy.get('div.book-item').should('have.length', 3);
         cy.get('div.book-item').should((books) => {
-            expect(books).to.have.length(2);
+            expect(books).to.have.length(3);
             const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design']);
+            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building Microservices']);
         });
     });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography } from '@mui/material';
+import axios from 'axios';
 import BookList from './BookList';
 
-function App() {
-  const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
-  return (
-    <div>
-      <Typography variant='h2' component='h2' data-test='heading' >
-        Bookish
-      </Typography>;
-      <BookList books={books} />
-    </div>
-  );
+const App = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(()=> {
+    const fetchBooks = async () => {
+      const res = await axios.get('http://localhost:8080/books');
+      setBooks(res.data);
+    };
+    fetchBooks();
+  },[])
+	// const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
+	return (
+		<div>
+			<Typography variant='h2' component='h2' data-test='heading'>
+				Bookish
+			</Typography>
+			;
+			<BookList books={books} />
+		</div>
+	);
 }
 
 export default App;
