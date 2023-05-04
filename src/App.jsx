8e5b66dafa706e0ceeefbd890346489bbcd52c64
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-05-04 17:54:28 -0300

    refactor: tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 			<Typography variant='h2' component='h2' data-test='heading'>
 				Bookish
 			</Typography>
-		<BookListContainer />
+			<BookListContainer />
 		</div>
 	);
 };
---
+++ src/teste.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Teste = () => {
+  return <>
+    <div>
+      <span>Testando</span>
+    </div>
+  </>
+}
+
+export default Teste;
